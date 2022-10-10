7fa55fd7f2fd206e97c4886f9cbd3f73ca461531
taturo2001 <92012553+taturo2001@users.noreply.github.com>
2022-10-10 22:53:46 +0700

    first time

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+const db = require('./config/db');
+
+const bodyParser = require('body-parser');
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
+app.use(bodyParser.json());
+
+app.use(methodOverride('_method'))
+
+// DB
+db.connect();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        helpers: {
+           sum: (a, b) => a + b,
+        }
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+route(app);
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
